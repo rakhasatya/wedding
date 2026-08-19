5fb21e9867e3778eae12a2c0928f0fb7f92aa18a
rakhasatya mahardhika <rakhasatya.pangestu@dayalima.id>
2026-08-19 22:28:28 +0700

    Production hardening: Open Graph metadata, mobile 100dvh fixes, and SEO optimization

--- app/layout.js
+++ app/layout.js
@@ -1,28 +1,49 @@
 import './globals.css';
 
 export const metadata = {
-  title: 'The Wedding Lunch of Fatimah & Rakhasatya',
-  description: 'Undangan Pernikahan Digital Fatimah & Rakhasatya - Sabtu, 12 Desember 2026',
+  metadataBase: new URL('https://wedding.rakhasatya.com'),
+  title: 'The Wedding Lunch of Fatimah Azzahra & Rakhasatya Mahardhika Pangestu',
+  description: 'Undangan Pernikahan Digital Fatimah Azzahra & Rakhasatya Mahardhika Pangestu - Sabtu, 29 Agustus 2026',
+  keywords: ['Fatimah Azzahra', 'Rakhasatya Mahardhika Pangestu', 'Wedding Invitation', 'Undangan Pernikahan'],
+  authors: [{ name: 'Fatimah & Rakhasatya' }],
+  themeColor: '#5c141c',
   openGraph: {
-    title: 'The Wedding Lunch of Fatimah & Rakhasatya',
-    description: 'Kami mengundang Bapak/Ibu/Saudara/i untuk merayakan hari kebahagiaan kami.',
+    title: 'The Wedding Lunch of Fatimah Azzahra & Rakhasatya Mahardhika Pangestu',
+    description: 'Kami mengundang Bapak/Ibu/Saudara/i untuk merayakan hari kebahagiaan kami pada Sabtu, 29 Agustus 2026.',
+    url: 'https://wedding.rakhasatya.com',
     siteName: 'Wedding of Fatimah & Rakhasatya',
     locale: 'id_ID',
     type: 'website',
+    images: [
+      {
+        url: '/og-image.jpg',
+        width: 1200,
+        height: 630,
+        alt: 'Undangan Pernikahan Fatimah Azzahra & Rakhasatya Mahardhika Pangestu',
+      },
+    ],
   },
-  icons: {
-    icon: '/favicon.ico',
+  twitter: {
+    card: 'summary_large_image',
+    title: 'The Wedding Lunch of Fatimah Azzahra & Rakhasatya Mahardhika Pangestu',
+    description: 'Undangan Pernikahan Digital - Sabtu, 29 Agustus 2026',
+    images: ['/og-image.jpg'],
+  },
+  robots: {
+    index: true,
+    follow: true,
   },
 };
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="id">
+    <html lang="id" className="scroll-smooth">
       <head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+        <meta name="theme-color" content="#5c141c" />
       </head>
-      <body className="bg-ivory-100 text-stone-800 antialiased selection:bg-maroon-800 selection:text-white">
+      <body className="bg-ivory-100 text-stone-800 antialiased selection:bg-maroon-800 selection:text-white min-h-[100dvh]">
         {children}
       </body>
     </html>
